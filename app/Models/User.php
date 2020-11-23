136efb3fe6224c107129fbb0b6e241f69179313c
Wojciech Sołtysiak <wojciech.soltysiak@actio.pl>
2020-11-23 14:28:51 +0100

    initial redis setup

---
+++ app/Models/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    static function onlineUsers() {
+        // return Cache::has('user-is-onlie-' . $this->id);
+
+        ##############################################
+
+        // Get the array of users
+        $users = Cache::get('online-users');
+        if(!$users) return null;
+
+        // Add the array to a collection so you can pluck the IDs
+        $onlineUsers = collect($users);
+        // Get all users by ID from the DB (1 very quick query)
+        $dbUsers = User::find($onlineUsers->pluck('id')->toArray());
+
+        // Prepare the return array
+        $displayUsers = [];
+
+        // Iterate over the retrieved DB users
+        foreach ($dbUsers as $user){
+            // Get the same user as this iteration from the cache
+            // so that we can check the last activity.
+            // firstWhere() is a Laravel collection method.
+            $onlineUser = $onlineUsers->firstWhere('id', $user['id']) ;
+            // Append the data to the return array
+            $displayUsers[] = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'mail' => $user->email,
+                // This Bool operation below, checks if the last activity
+                // is older than 3 minutes and returns true or false,
+                // so that if it's true you can change the status color to orange.
+                'away' => $onlineUser['last_activity_at'] < now()->subMinutes(3),
+            ];
+        }
+        return collect($displayUsers);
+    }
+}
